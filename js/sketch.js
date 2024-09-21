c25e8794df002d995e5a1e19cdd352fb5561edba
AlejandroMont201 <alejomont25@gmail.com>
2024-09-21 19:34:15 -0400

    commit3

--- js/sketch.js
+++ js/sketch.js
@@ -1,7 +1,7 @@
 let angle = 0; // Variable para el ángulo de rotación
 
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 600); // Aumentamos el tamaño del lienzo para que quepan las tres flores
   textFont('Georgia'); // Usar la fuente Georgia
   textSize(32);   // Tamaño de letra
   textAlign(CENTER, CENTER); // Centrar el texto
@@ -12,19 +12,31 @@ function draw() {
   background(220); // Limpiar el lienzo en cada frame
 
   // Dibujar el mensaje en la parte superior
-  fill(225, 180, 0); // Color del texto
+  fill(225, 180, 0); // Color dorado
   text("Feliz día, Manfred", width / 2, 40); // Texto centrado en la parte superior
+
+  // Dibujar las tres flores
+  drawFlower(150, 350);  // Flor 1
+  drawFlower(300, 400);  // Flor 2
+  drawFlower(450, 350);  // Flor 3
+
+  // Animar el giro de las flores
+  angle += 0.01;
+}
+
+// Función para dibujar una flor en la posición (x, y)
+function drawFlower(x, y) {
+  push();
+  translate(x, y);  // Mover la flor a la posición indicada
 
   // Dibujar el tallo verde con un degradado simple
   noStroke();
   for (let i = 0; i < 150; i++) {
     let inter = map(i, 0, 150, 0.6, 1); // Gradiente entre dos colores
     fill(34 * inter, 139 * inter, 34 * inter);
-    rect(195, 200 + i, 10, 1); // Tallo degradado
+    rect(-5, 0 + i, 10, 1); // Tallo degradado
   }
 
-  // Dibujar la flor
-  translate(200, 200);
   rotate(angle); // Rotar la flor
 
   // Sombra de la flor para darle profundidad
@@ -49,6 +61,5 @@ function draw() {
     ellipse(0, 0, r * 2, r * 2); // Círculos concéntricos
   }
 
-  // Animar el giro de la flor
-  angle += 0.01;
+  pop();
 }
